f5d1bb54e17879d6ac755b04c90cf1db88e3a558
Apprentice <apprentice@dbc28.local>
2014-06-06 11:09:42 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootOverflow::Application.config.secret_key_base = 'bca1a71a51939cce2ebacb96d4d6b1c9f21ddccd0026419577ee5bebc041f75420f4aee09b7bdd8ea0c35e8ad3fd719b201616c61cc1f58923b4411da5cc6515'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BootOverflow::Application.config.session_store :cookie_store, key: '_boot_overflow_session'
