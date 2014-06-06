45dae3985339599398b481c131b4c5efe21af4e6
Chris DeLauder & Sean Dickey <pair+cdelauder.sdickey@devbootcamp.com>
2014-06-06 11:28:49 -0700

    built routes and controllers, havent made the view yet

---
+++ app/controllers/concerns/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if User.find(@user)
+      redirect_to user_path
+    else
+      render 'new'
+  end
+end
