ac36fb37c1e9e0b25b8290c2d7180c9402e81769
Chris DeLauder & Sean Dickey <pair+cdelauder.sdickey@devbootcamp.com>
2014-06-06 14:32:25 -0700

    Created questions model, controller, migration and index route.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ActiveRecord::Base
-  def index
-    if session[:id]
-      @user = User.find(session[:id])
-    else
-      redirect_to new_sessions_path
-    end
-  end
+  has_many :questions
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ BootOverflow::Application.routes.draw do
   root 'users#index'
   resources :users, :only => [:index]
   resources :sessions, :only => [:new, :create]
+  resources :questions, :only => [:index]
 end
