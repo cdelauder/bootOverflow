4c68bacb7bb52d0c2e9a442c890e1fa6ae1d2293
Apprentice <apprentice@dbc28.local>
2014-06-06 16:05:19 -0700

    login working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 BootOverflow::Application.routes.draw do
-
   get 'sign_up'=> 'users#new', :as => 'sign_up'
   get 'log_in' => 'sessions#new', :as => 'log_in'
   get 'log_out' => 'sessions#destroy', :as => 'log_out'
