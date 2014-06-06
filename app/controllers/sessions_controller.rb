e2344b2bba6ecfd3d492f4a994bf10b20a1fa81b
Apprentice <apprentice@dbc28.local>
2014-06-06 12:00:48 -0700

    created login sessions for user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,20 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
+  def create
+    user = User.authenticate(params[:email], params[:password])
 
+    if user
+      session[:user_id] = user.id
+      redirect_to 'users#index'
+    else
+      redirect_to 'sessions#new'
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to 'sessions#new'
+  end
 end
