dad0dc738cf78df5c3ebf7f1ca7f573481316908
Apprentice <apprentice@dbc29.local>
2014-06-07 14:08:38 -0700

    answers edit and delete are working

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -15,11 +15,29 @@ class AnswersController < ApplicationController
   end
 
   def edit
+    @question = Question.find params[:question_id]
+    @answer = @question.answers.find params[:id]
   end
 
   def update
+    @question =Question.find params[:question_id]
+    @answer = @question.answers.find params[:id]
+
+    if @answer.update_attributes params[:answer]
+      redirect_to questions_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    p params[:question_id]
+    p params[:id]
+
+    question = Question.find params[:question_id]
+    answer = question.answers.find params[:id]
+
+    answer.destroy
+    redirect_to questions_path
   end
 end
