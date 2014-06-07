0953a299fe185f65d74140e917f243f1fb7bb9a7
Chris DeLauder & Sean Dickey <pair+cdelauder.sdickey@devbootcamp.com>
2014-06-06 19:26:44 -0700

    log out functionality has been implemented, and links to make navigation through pages have been created

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -9,18 +9,20 @@ describe QuestionsController do
   end
 
   context '#show' do
+    let(:user) {email: 'a@a.com', password: 'a', password_confirmation: 'a'}
+    let (:question) {title: "This thing here", content: "this is content", user_id: 1}
     it 'should display a single question' do
-      question = Question.create(title: "This thing here", content: "this is content", user_id: 1)
-      get 'show', :id => question
+      Question.create(:question)
+      get 'show', :id => :user.id
       expect(assigns(:question)).to eq(Question.last)
     end
   end
 
   context '#delete' do
     it 'should delete a question' do
-      question = Question.create(title: "Another Question", content: "Test", user_id: 1)
-      post 'destroy', :id => question
+      Question.create(:question)
+      post 'destroy', :id => :user.id
       expect(Question.last).to not_eq(question)
     end
   end
-end
+endz
